30c07e4758fa2cae4cf0ea12bd56f9b0b55c5399
Cayden Kappes <caydenkappes@gmail.com>
2023-04-19 23:38:49 -0700

    Working(ish)

--- EagleTracks/components/CheckStockPage.js
+++ EagleTracks/components/CheckStockPage.js
@@ -6,48 +6,32 @@ import itemList from '../itemList.json'
 import login from '../loginCred.json'
 
 const ReturnPage = ({navigation, style}) => {
-    const [numToIncr, setNumToIncr] = useState('');
-    const [itemToIncr, setItemToIncr] = useState('');
+    
     const [formValues, setFormValues] = useState([{ itemToCheck: "", numNeeded : ""}]);
     
 
     
     const checkFields = () => {
-      if(!(numToIncr.trim())){
-        alert('Number is empty');
-      }else if(!(parseInt(numToIncr))){
-        alert('Returning non-number quantities is not supported at this time');
-      }else if(!(itemToIncr.trim())){
-        alert('Item is empty');
-      }else if(!(login['accessToken'])){
-        alert('Missing access token.  Please enter your access token.')
-        navigation.navigate("Change Credentials")
-      }
-      else{
-        let itemID = checkFiles(itemToIncr)
-        if(!itemID.trim()){
-          alert("ItemID not present, please add the item")
-        }else{
-          API.incr(itemID, parseInt(numToIncr), navigation);
-          navigation.navigate('Working Page');
-        }
+      for(i = 0; i < formValues.length; i++){
+        console.log(formValues[i])
       }
     }
     
 
-    let handleChange = (index, e) => {
-        /*let name = e.target[name]
-        let value = e.target.value
-        //console.log(e.target)
-        console.log(e.target.name)
-        let list = [...formValues]
-        list[index][name] = value
-        setFormValues(list)*/
+    let handleChangeItem = (text, index) => {
 
-        let newFormValues = [...formValues];
-        newFormValues[index][e.target.name] = e.target.value;
-        setFormValues(newFormValues);
+        const _formValues = [...formValues]
+        _formValues[index].itemToCheck = text;
+        console.log(text)
+        setFormValues(_formValues)
       }
+
+    let handleChangeAmount = (text, index) => {
+        const _formValues = [...formValues]
+        _formValues[index].numNeeded = text;
+        console.log(text)
+        setFormValues(_formValues)
+    }
     
     let addFormFields = () => {
         setFormValues([...formValues, { itemToCheck: "", numNeeded: "" }])
@@ -67,11 +51,11 @@ const ReturnPage = ({navigation, style}) => {
                 <Text></Text>
               <Text>Item ID</Text>
               <View>
-              <TextInput style={style.input} name="itemToCheck" value={element.itemToCheck || ''} onChange={e => handleChange(index, e)} />
+              <TextInput style={style.input} value={element.value} onChangeText={text => handleChangeItem(text, index)} />
               </View>
               <Text>Amount Needed</Text>
               <View>
-              <TextInput style={style.input} name="numNeeded" value={element.numNeeded || ''} onChange={e => handleChange(index, e)} />
+              <TextInput style={style.input} value={element.value} onChangeText={text => handleChangeAmount(text, index)} />
               </View>
               {
                 index ? 
