5e570a4e95f664f7cdad934ed2237037bd337d55
TheMadMystic <65517955+TheMadMystic@users.noreply.github.com>
2023-06-05 12:00:17 -0700

    Small update from attempting to generate csvs

--- EagleTracks/components/HomePage.js
+++ EagleTracks/components/HomePage.js
@@ -2,6 +2,28 @@ import React from 'react';
 import {Text, View, Button, StatusBar, Alert} from 'react-native';
 import {sendEmail} from '../helperFunctions.js';
 
+import * as FileSystem from 'expo-file-system'; //must use npx expo install expo-file-system
+//import csv from './temp.csv';
+
+//set up for csv
+//createCSV("col1,col2,col3\n1,2,3\n4,5,6");
+//const fileDir = FileSystem.documentDirectory + 'files/';
+//const csvFileURI = fileDir + 'temp.csv';
+//const csvInfo = await FileSystem.getInfoAsync(csvFileURI);
+//const csvInfo = 
+const saveFile = async (data) => {
+
+  let directoryUri = FileSystem.documentDirectory;
+  
+  let fileUri = directoryUri + "temp.csv";
+  
+  await FileSystem.writeAsStringAsync(fileUri, data, { encoding: FileSystem.EncodingType.UTF8 });
+  
+  return fileUri;
+};
+
+const csvURI = saveFile("col1,col2,col3\n1,2,3\n4,5,6\n7,8,9");
+
 const HomePage = ({navigation, style}) => {
     return(
       <View style={style.container}>
@@ -13,7 +35,7 @@ const HomePage = ({navigation, style}) => {
         <Text></Text>
         <Button style={style.buttonStyle} onPress={() => navigation.navigate('Add New Item')} title="Add New Item" color="#a10022"/>
         <Text></Text>
-        <Button style={style.buttonStyle} onPress={() => sendEmail(subject='Test', recipients=['awallace8@ewu.edu','a.j.wallace@hotmail.com'], body='This is a test file.')} title="Send Email" color="#a10022"/>
+        <Button style={style.buttonStyle} onPress={() => sendEmail(subject='Test', recipients=['awallace8@ewu.edu','a.j.wallace@hotmail.com'], body='This is a test file.', attachments=[])} title="Send Email" color="#a10022"/>
         <StatusBar style="auto" />
       </View>
     )
--- EagleTracks/helperFunctions.js
+++ EagleTracks/helperFunctions.js
@@ -3,10 +3,34 @@ Purpose: Defines a set of functions to be used by other components
 
 */
 import * as MailComposer from 'expo-mail-composer'; //requires: npm install expo-mail-composer
+//import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+//import RNFS from 'react-native-fs'; //requires npm install react-native-fs
+//import { useState, useEffect } from 'react';
+//import * as FileSystem from 'expo-file-system'; //must use npx expo install expo-file-system
+
+//const filePath = RNFS.DocumentDirectoryPath + "/temp.csv";
+
+// createCSV does as the name implies
+//must be used in a useEffect in a component before the file is to be called.
+/* const createCSV = async(values) => {
+    try{
+        await RNFS.writeFile(filePath, values, "utf8");
+        console.log("Created csv file");
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+const getCSVpath = async() => {
+    return filePath;
+} */
 
 // sendEmail() is adapted from tutorial code from this page https://levelup.gitconnected.com/expo-react-native-email-with-attachment-d6a677b6d864
 const sendEmail = async (subject, recipients, body, attachments=[]) =>{
     var contents = {}
+    const [status,setStatus] = useState(null);
     if(attachments.length < 1){
         contents =
         {
@@ -37,9 +61,12 @@ const sendEmail = async (subject, recipients, body, attachments=[]) =>{
             })
     })
     promise.then(
-        result => setStatusBarBackgroundColor("Status: email " + result.status),
-        error => setStatusBarBackgroundColor("Status: email " + error.status)
+        result => setStatus("Status: email " + result.status),
+        error => setStatus("Status: email " + error.status)
     )
     
 }
+
 export {sendEmail}
+//export {createCSV}
+//export {getCSVpath}
